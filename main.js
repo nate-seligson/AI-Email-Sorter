5de440030564aaa700210621ac211e7f337d2ff4
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2024-04-30 21:49:28 -0400

    prompt improvement

--- main.js
+++ main.js
@@ -7,10 +7,16 @@ fetch("api-key.txt")
     const info = document.getElementById("info")
     var xhr = new XMLHttpRequest();
     let emails = []
+    let emailLength = ""
     xhr.onreadystatechange = function() {
       if(xhr.responseText.length > 5 && emails.length == 0){
         emails = xhr.responseText.split("~||~");
-        info.innerHTML = ("gotten " + emails.length + " new emails...")
+        emailLength = emails.length.toString()
+        if(emails.length > 10){
+          emailLength = "10+"
+          emails = emails.slice(0,9)
+        }
+        info.innerHTML = ("gotten " + emailLength + " new emails...")
         run(emails)
       }
     };
@@ -22,7 +28,7 @@ fetch("api-key.txt")
         // For text-only input, use the gemini-pro model
         const model = genAI.getGenerativeModel({ model: "gemini-pro", temperature: 0.9});
         for(var i = 0; i<emails.length; i++){
-          const prompt = "Take an input of an email and rate it on a scale of 0-100 on how urgent it is to respond to, and summarize it including the sender in 10 words or less.Format it like [7, \"Spam from New York Times\"][92, \"Financial Aid offer From MIT\"] [31, \"English Assignment due in 5 weeks\"], [57, \"AP Meeting Signup from Math Teacher\"]" + emails[i]
+          const prompt = "Take an input of an email and rate it on a scale of 0-100 on how urgent it is to respond to, and summarize it including the sender in 10 words or less Format it like this: Input: Read new york times and get 20% off your next subscription! Output: [0, \"Spam from New York Times\"] Input: Congratiolations on your acceptace to our school Output: [99, \"College acceptance letter\"], Input: Hello, just RSVPing to remind you that this internship expires in two days. Output: [91, \"Internship offer that expires in two days\"], Input: Hi, can we schedule a meeting this week? I want to talk to you about xyz. Output: [86, \"Meeting schediling for this week\"] Input: English teacher posted a new assignment due in 5 weeks. Output: [31, \"English Assignment due in 5 weeks\"], Input: Please read the signup sheet to signup for Math AP Meetings. Output: [57, \"AP Meeting Signup from Math Teacher\"], but make it clearly based on the input email. Emphasize things that are sent by real humans, scheduling things etc, and reponses, and vitally important info, and de-emphasize spam. Here is the email you have to summarize:" + emails[i]
         
           const result = await model.generateContent(prompt);
           const response = await result.response;
@@ -34,7 +40,7 @@ fetch("api-key.txt")
           taskEmailDict[JSON.parse(text)] = emails[i]
           }
           catch{console.log(text)}
-          info.innerHTML = ("gotten " + emails.length + " new emails--") + "\n Loading..." + (i/emails.length) * 100 + "%"
+          info.innerHTML = ("gotten " + emailLength + " new emails--") + "\n Loading..." + (i/emails.length) * 100 + "%"
         }
         info.style.display = "none"
         taskslist.sort((a,b) => b[0]-a[0]);
